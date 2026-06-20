5607eace4589d38d6731943ab467253ad0c6071a
PENGEANG EAN <pengeangean@gmail.com>
2026-06-20 22:51:45 +0700

    Create studentsData.ts

---
+++ src/studentsData.ts
@@ -0,0 +1,113 @@
+import { Student } from "./types";
+
+export const initialStudentsList: Student[] = [
+  {
+    id: "s-01",
+    name: "អ៊ាន ប៉េងអ៊ាង",
+    gender: "ប្រុស",
+    dob: "2004-10-12",
+    address: "សង្កាត់វាលវង់ ក្រុងកំពង់ចាម ខេត្តកំពង់ចាម",
+    phoneNumber: "0964567123",
+    telegram: "@Pengeang_Ean",
+    isMonitor: true
+  },
+  {
+    id: "s-02",
+    name: "លី ម៉េងហ៊ួ",
+    gender: "ប្រុស",
+    dob: "2003-05-18",
+    address: "ភូមិជើងព្រៃ ស្រុកបាធាយ ខេត្តកំពង់ចាម",
+    phoneNumber: "0886571234",
+    telegram: "@menghour_ly"
+  },
+  {
+    id: "s-03",
+    name: "សួង ស្រីពៅ",
+    gender: "ស្រី",
+    dob: "2004-02-14",
+    address: "ឃុំគគីរធំ ស្រុកចំការលើ ខេត្តកំពង់ចាម",
+    phoneNumber: "077556612",
+    telegram: "@sreypov_soung"
+  },
+  {
+    id: "s-04",
+    name: "ចាន់ សុភ័ក្ត្រ",
+    gender: "ប្រុស",
+    dob: "2003-11-30",
+    address: "ភូមិទីមួយ ក្រុងកំពង់ចាម ខេត្តកំពង់ចាម",
+    phoneNumber: "0978899123",
+    telegram: "@sopheak_chan"
+  },
+  {
+    id: "s-05",
+    name: "ឈិន ចាន់ដារ៉ា",
+    gender: "ប្រុស",
+    dob: "2004-08-05",
+    address: "ឃុំត្រពាំងព្រះ ស្រុកព្រៃឈរ ខេត្តកំពង់ចាម",
+    phoneNumber: "010998811",
+    telegram: "@dara_chhin"
+  },
+  {
+    id: "s-06",
+    name: "ឃួន ស្រីនី",
+    gender: "ស្រី",
+    dob: "2004-04-25",
+    address: "ឃុំរំចេក ស្រុកកំពង់សៀម ខេត្តកំពង់ចាម",
+    phoneNumber: "0962345678",
+    telegram: "@sreyni_khuon"
+  },
+  {
+    id: "s-07",
+    name: "តុប វឌ្ឍនៈ",
+    gender: "ប្រុស",
+    dob: "2003-09-12",
+    address: "ភូមិព្រែកព្នៅ ស្រុកកោះសូទិន ខេត្តកំពង់ចាម",
+    phoneNumber: "085345678",
+    telegram: "@vathanak_tob"
+  },
+  {
+    id: "s-08",
+    name: "ម៉ៅ សុធារី",
+    gender: "ស្រី",
+    dob: "2004-12-01",
+    address: "ភូមិតាសែន ស្រុកស្រីសន្ធរ ខេត្តកំពង់ចាម",
+    phoneNumber: "012885544",
+    telegram: "@sotheary_mao"
+  },
+  {
+    id: "s-09",
+    name: "ហេង សុវណ្ណារ៉ា",
+    gender: "ប្រុស",
+    dob: "2004-01-15",
+    address: "សង្កាត់សំបួរមាស ក្រុងកំពង់ចាម ខេត្តកំពង់ចាម",
+    phoneNumber: "0977881122",
+    telegram: "@sovannara_heng"
+  },
+  {
+    id: "s-10",
+    name: "អ៊ឹម សាវី",
+    gender: "ស្រី",
+    dob: "2003-07-22",
+    address: "ឃុំសូភាស ស្រុកស្ទឹងត្រង់ ខេត្តកំពង់ចាម",
+    phoneNumber: "015443322",
+    telegram: "@savy_im"
+  },
+  {
+    id: "s-11",
+    name: "សុខ ធីតា",
+    gender: "ស្រី",
+    dob: "2004-06-19",
+    address: "ភូមិខ្សាច់ប្រទិល ស្រុកកោះសូទិន ខេត្តកំពង់ចាម",
+    phoneNumber: "0963322114",
+    telegram: "@thida_sok"
+  },
+  {
+    id: "s-12",
+    name: "នួន ចាន់ធូ",
+    gender: "ប្រុស",
+    dob: "2003-03-08",
+    address: "ឃុំខ្វិតធំ ស្រុកព្រៃឈរ ខេត្តកំពង់ចាម",
+    phoneNumber: "0889988776",
+    telegram: "@chanthou_nuon"
+  }
+];
